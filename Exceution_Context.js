d80916d73682ce117a753ff8cf1873b287388e5f
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-18 21:18:33 -0700

    Code Successfully

---
+++ Exceution_Context.js
@@ -0,0 +1,12 @@
+
+// ********* JavaScript Execution Context ******** //
+// Global :
+{ } // this curley brasis is called global EC. and used in *this* property.
+
+// The value of this in browser is called *window object*.
+
+// **** Global Execution Context **** //
+
+// Function Execution Context
+// Eval Execution Context
+
